2eede57e0e3fa6eb6b3ba2c7ce6c5dcfce520ad7
Sarthak Deshmukh <sarthak.deshmukh@yhworks.com>
2025-08-02 07:12:17 +0530

    Initial Commit

---
+++ RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Sarthak Deshmukh on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
